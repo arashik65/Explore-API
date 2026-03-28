749c0931e6b61801394c556e5050f5649858d1a2
arashik65 <mdashikur6398+@gmail.com>
2026-03-28 19:32:51 +0600

    todo json in api

---
+++ todo.js
@@ -0,0 +1,38 @@
+const loodTodo=()=>{
+   const url="https://jsonplaceholder.typicode.com/todos";
+   fetch(url)
+   .then((res)=>res.json())
+   .then((data)=> displayTodo(data));
+};
+
+// {
+//     "userId": 10,
+//     "id": 194,
+//     "title": "sed ut vero sit molestiae",
+//     "completed": false
+// }
+
+const displayTodo=(todos)=>{
+  //1.get the container and empty the container
+  const todoContainer = document.getElementById("todo-container");
+  todoContainer.innerHTML="";
+
+  todos.forEach(todo=>{
+    console.log(todo);
+    const todoCard=document.createElement("div");
+    todoCard.innerHTML=`
+    <div class="todo-card">
+       
+       <p>${
+         todo.completed ==true 
+         ? `<i class="fa-solid fa-square-check"></i>`
+         : `<i class="fa-regular fa-square-check"></i>`
+      }</p>
+      <h4>${todo.title}</h4>
+    </div>
+    `;
+    todoContainer.append(todoCard);
+  });
+};
+
+loodTodo();
